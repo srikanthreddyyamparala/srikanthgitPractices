43fb7f128fb0b4f1327fa6f1601dfc024d2d26d1
SRIKANTHREDDY <SRIKANTHREDDY@SrikanthReddy.lan>
2020-04-14 16:53:35 +0530

    New Java program created by srikanth reddy

---
+++ DemoProj1/src/com/srikanth/test/Hello.java
@@ -0,0 +1,11 @@
+package com.srikanth.test;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	 System.out.println("8184845258");
+	 System.out.println("srikanthreddy");
+	}
+	
+
+}
