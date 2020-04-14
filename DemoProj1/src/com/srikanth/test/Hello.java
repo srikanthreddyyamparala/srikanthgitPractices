915c36ad6ee934a5a7351a152982bf2868c9281d
SRIKANTHREDDY <SRIKANTHREDDY@SrikanthReddy.lan>
2020-04-14 17:00:43 +0530

    Hello program modified by Lokesh reddy

--- DemoProj1/src/com/srikanth/test/Hello.java
+++ DemoProj1/src/com/srikanth/test/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 	 System.out.println("8184845258");
 	 System.out.println("srikanthreddy");
+	 System.out.println("lokesh reddy");
 	}
 	
 
