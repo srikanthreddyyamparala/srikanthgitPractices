6fe12ff4e3629bef0537dc9f6aa68c39b4e12cdd
SRIKANTHREDDY <SRIKANTHREDDY@SrikanthReddy>
2020-04-15 06:02:17 +0530

    Hello program has been updated by Srikanth Reddy

--- DemoProj1/src/com/srikanth/test/Hello.java
+++ DemoProj1/src/com/srikanth/test/Hello.java
@@ -6,6 +6,9 @@ public class Hello {
 	 System.out.println("8184845258");
 	 System.out.println("srikanthreddy");
 	 System.out.println("lokesh reddy");
+	 System.out.println("8184989881");
+	 System.out.println("y.s.reddy597@gmail.com");
+	 System.out.println("lokeshreddyyamparala@gmail.com");
 	}
 	
 
